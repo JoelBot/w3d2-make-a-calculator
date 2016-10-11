02605f6029b0d2e4ec94721cc29a89410ae1d106
JoelBot <Joel.Lonneker@gmail.com>
2016-10-11 13:57:47 -0400

    adventure time for calculator

--- js/calc.js
+++ js/calc.js
@@ -12,6 +12,15 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
     return num1 / num2
 }
+function remainder (num1, num2) {
+    return num1 % num2
+}
+function increment (num1) {
+    return num1 + 1
+}
+function decrement (num1) {
+    return num1 - 1
+}
 
 answer = add(40, 110)
 console.assert(answer === 150)
@@ -21,3 +30,9 @@ answer = divide(answer, 6)
 console.assert(answer === 23.5)
 answer = multiply(answer, 2)
 console.assert(answer === 47)
+answer = remainder(20, 9)
+console.assert(answer === 2)
+answer = increment(20)
+console.assert(answer === 21)
+answer = decrement(20)
+console.assert(answer === 19)
