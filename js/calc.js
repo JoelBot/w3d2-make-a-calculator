46eb9438b2a98f4e6ea4bcddddbabec42716e0f7
JoelBot <Joel.Lonneker@gmail.com>
2016-10-11 22:37:37 -0400

    final commit with calculator things

--- js/calc.js
+++ js/calc.js
@@ -22,17 +22,55 @@ function decrement (num1) {
     return num1 - 1
 }
 
-answer = add(40, 110)
-console.assert(answer === 150)
-answer = subtract(answer, 9)
-console.assert(answer === 141)
-answer = divide(answer, 6)
-console.assert(answer === 23.5)
-answer = multiply(answer, 2)
-console.assert(answer === 47)
-answer = remainder(20, 9)
-console.assert(answer === 2)
-answer = increment(20)
-console.assert(answer === 21)
-answer = decrement(20)
-console.assert(answer === 19)
+// answer = add(40, 110)
+// console.assert(answer === 150)
+// answer = subtract(answer, 9)
+// console.assert(answer === 141)
+// answer = divide(answer, 6)
+// console.assert(answer === 23.5)
+// answer = multiply(answer, 2)
+// console.assert(answer === 47)
+// answer = remainder(20, 9)
+// console.assert(answer === 2)
+// answer = increment(20)
+// console.assert(answer === 21)
+// answer = decrement(20)
+// console.assert(answer === 19)
+
+document.getElementById('addButton').addEventListener('click', function() {
+    var firstValue = Number(document.getElementById('firstValue').value)
+    var secondValue = Number(document.getElementById('secondValue').value)
+
+    var answer = add(firstValue, secondValue)
+
+    document.getElementById('firstValue').value = answer
+})
+document.getElementById('subtractButton').addEventListener('click', function() {
+    var firstValue = Number(document.getElementById('firstValue').value)
+    var secondValue = Number(document.getElementById('secondValue').value)
+
+    var answer = subtract(firstValue, secondValue)
+
+    document.getElementById('firstValue').value = answer
+})
+document.getElementById('multiplyButton').addEventListener('click', function() {
+    var firstValue = Number(document.getElementById('firstValue').value)
+    var secondValue = Number(document.getElementById('secondValue').value)
+
+    var answer = multiply(firstValue, secondValue)
+
+    document.getElementById('firstValue').value = answer
+})
+document.getElementById('divideButton').addEventListener('click', function() {
+    var firstValue = Number(document.getElementById('firstValue').value)
+    var secondValue = Number(document.getElementById('secondValue').value)
+
+    var answer = divide(firstValue, secondValue)
+
+    document.getElementById('firstValue').value = answer
+})
+document.getElementById('clearButton').addEventListener('click', function() {
+
+    document.getElementById('firstValue').value = ''
+    document.getElementById('secondValue').value = ''
+})
